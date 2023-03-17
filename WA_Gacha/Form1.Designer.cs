26d32b51a84893e371f8f3e7879ec2c2d8742129
Liny <d.moonlin@gmail.com>
2023-03-17 19:25:38 +0800

    加入專案檔案。

---
+++ WA_Gacha/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WA_Gacha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(128, 96);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(550, 31);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(705, 96);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "投幣";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox2.Location = new System.Drawing.Point(128, 162);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(694, 193);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.Location = new System.Drawing.Point(702, 378);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 39);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "出貨";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(560, 378);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 39);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "退幣";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(999, 454);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WA_Gacha/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace WA_Gacha
+{
+    public partial class Form1 : Form
+    {
+        public const string Pattern = @"^[0-9]*$";
+        public static decimal Total = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FormViewModel model = BindModel();
+
+            decimal money;
+            GetCoin();
+            decimal.TryParse(GetCoin(), out money);
+            model.Money = money;
+            Total += model.Money;
+            GenerateStatus(Total);
+        }
+
+        private void GenerateStatus(decimal money)
+        {
+            if (money > 0 && money < 50)
+            {
+                button2.Enabled = true;
+                textBox2.Text = string.Format(@"State:InsufficientAmountState 有投幣,但金額還不足夠" + "\r\n" + "Money:{0}", money);
+                return;
+            }
+            if (money >= 50)
+            {
+                button2.Enabled = true;
+                button3.Enabled = true;
+
+                textBox2.Text = string.Format(@"State:ReadyToShip 投入足夠金額,等待客人指令就出貨" + "\r\n" + "Money:{0}", money);
+                return;
+            }
+
+            textBox2.Text = string.Format(@"State:StandbyState 機器沒在工作,等待下一個客人操作" + "\r\n" + "Money:{0}", money);
+        }
+
+        private string GetCoin()
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                return 0.ToString();
+
+            }
+            if (textBox1.Text.Length <= 0)
+            {
+                return 0.ToString(); //_model.Money = 0;
+            }
+            if (Regex.IsMatch(textBox1.Text, Pattern) == false)
+            {
+                return 0.ToString();
+            }
+
+            if (int.Parse(textBox1.Text) <= 0)
+            {
+                return 0.ToString();
+            }
+            return textBox1.Text;
+        }
+
+        private FormViewModel BindModel()
+        {
+            var model = new FormViewModel();
+            model.Coin = textBox1.Text;
+            model.State = textBox2.Text;
+            //model.Money +=  model.Coin;
+            return model;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            Total -= Total;
+            GenerateReturnStatus(Total);
+        }
+
+        private void GenerateReturnStatus(decimal money)
+        {
+            MessageBox.Show(@"已完成退幣作業");
+            button2.Enabled = false;
+            button3.Enabled = false;
+
+            textBox2.Text = string.Format(@"State:StandbyState 機器沒在工作,等待下一個客人操作 " + "\r\n" + "Money:{0}", money);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Total = Total - 50;
+            GenerateShippedStatus(Total);
+        }
+
+        private void GenerateShippedStatus(decimal money)
+        {
+            var model = BindModel();
+            model.Money = money;
+           // model.Money = money - 50;
+            if (model.Money >= 50)
+            {
+                MessageBox.Show(@"已出貨");
+                button2.Enabled = true;
+                button3.Enabled = true;
+                textBox2.Text = string.Format(@"State:ReadyToShip 投入足夠金額,等待客人指令就出貨" + "\r\n" + "Money:{0}", model.Money);
+                return;
+            
+            }else if (model.Money > 0 && model.Money < 50)
+            {
+                button2.Enabled = true;
+                button3.Enabled = false;
+                textBox2.Text = string.Format(@"State:InsufficientAmountState 有投幣,但金額還不足夠" + "\r\n" + "Money:{0}", model.Money);
+            }
+            else
+            {
+                button2.Enabled = false;
+                button3.Enabled = false;
+
+                textBox2.Text = string.Format(@"State:ReadyToShip 投入足夠金額,等待客人指令就出貨" + "\r\n" + "Money:{0}",
+        model.Money);
+            }
+        }
+    }
+
+    public class FormViewModel
+    {
+        public string Coin { get; set; }
+        public string State { get; set; }
+        public decimal Money { get; set; }
+    }
+}
